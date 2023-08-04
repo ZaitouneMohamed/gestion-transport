d3f3e704369de1b3e2efd4b671dee9fe2bd70072
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-08-04 12:21:28 +0100

    update search function in livewire

--- app/Http/Livewire/GlobalSearch.php
+++ app/Http/Livewire/GlobalSearch.php
@@ -19,9 +19,11 @@ class GlobalSearch extends Component
     public function render()
     {
         return view('livewire.global-search', [
-            "trajets" => Consomation::where("camion_id", $this->camion)
-                ->orWhere("chaufeur_id",  $this->chaufeur)
-                ->orWhere("ville",  $this->destination)
+            "trajets" => Consomation::where(function ($query) {
+                $query->where("camion_id", $this->camion)
+                    ->orWhere("chaufeur_id",  $this->chaufeur)
+                    ->orWhere("ville",  $this->destination);
+            })
                 ->whereBetween('created_at', [$this->datedebut, $this->datefin])
                 ->get()
         ]);
