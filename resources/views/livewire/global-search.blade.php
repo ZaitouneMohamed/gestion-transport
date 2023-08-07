54c81e8348f6eda65f82b464950285cf1a7008c3
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-08-07 15:37:20 +0100

    Update global-search.blade.php

--- resources/views/livewire/global-search.blade.php
+++ resources/views/livewire/global-search.blade.php
@@ -90,54 +90,54 @@
         </thead>
         <tbody>
             @foreach ($trajets as $item)
-            <tr>
-                <th scope="row">{{ $item->id }}</th>
-                <td>{{ $item->chaufeur->full_name }}</td>
-                <td>{{ $item->camion->matricule }}</td>
-                <td>{{ $item->ville }}</td>
-                <td>
-                    @if ($item->Bons->where('nature', 'gazole')->count() >= 2)
-                        {{ $item->QtyLittre }}
-                    @endif
-                </td>
-                <td>
-                    @if ($item->Bons->where('nature', 'gazole')->count() > 1)
-                        {{ $item->KmTotal }}
-                    @endif
-                </td>
-                <td>
-                    @if ($item->Bons->where('nature', 'gazole')->count() > 1)
-                        {{ $item->Taux }}
-                    @endif
-                </td>
-                <td>
-                    {{ $item->Camion->consommation }}
-                </td>
-                <td>
-                    <span
-                        class="badge
+                <tr>
+                    <th scope="row">{{ $item->id }}</th>
+                    <td>{{ $item->chaufeur->full_name }}</td>
+                    <td>{{ $item->camion->matricule }}</td>
+                    <td>{{ $item->ville }}</td>
+                    <td>
+                        @if ($item->Bons->where('nature', 'gazole')->count() >= 2)
+                            {{ $item->QtyLittre }}
+                        @endif
+                    </td>
+                    <td>
+                        @if ($item->Bons->where('nature', 'gazole')->count() > 1)
+                            {{ $item->KmTotal }}
+                        @endif
+                    </td>
+                    <td>
+                        @if ($item->Bons->where('nature', 'gazole')->count() > 1)
+                            {{ number_format($item->Taux, 2) }}
+                        @endif
+                    </td>
+                    <td>
+                        {{ $item->Camion->consommation }}
+                    </td>
+                    <td>
+                        <span
+                            class="badge
                     @if ($item->statue > 0) bg-danger
                     @else
                     bg-success @endif
-                    ">{{ $item->Statue }}</span>
-                </td>
-                <td>
-                    {{ $item->Prix }}
-                </td>
-                <td class="d-flex">
-                    <a href="{{ route('createBon', $item->id) }}" title="Add Bons Here"
-                        class="btn btn-success mr-1"><b><i class="fa fa-plus"></i></b></a>
-                    <a href="{{ route('consomations.edit', $item->id) }}" class="btn btn-warning mr-1"><i
-                            class="fa fa-pen"></i></a>
-                    <a href="{{ route('getBons', $item->id) }}" class="btn btn-info mr-1"><i
-                            class="fa fa-eye"></i></a>
-                    <form action="{{ route('consomations.destroy', $item->id) }}" method="post">
-                        @csrf
-                        @method('delete')
-                        <button class="btn btn-danger"><i class="fa fa-trash"></i></button>
-                    </form>
-                </td>
-            </tr>
+                    ">{{ number_format($item->Statue, 2) }}</span>
+                    </td>
+                    <td>
+                        {{ $item->Prix }}
+                    </td>
+                    <td class="d-flex">
+                        <a href="{{ route('createBon', $item->id) }}" title="Add Bons Here"
+                            class="btn btn-success mr-1"><b><i class="fa fa-plus"></i></b></a>
+                        <a href="{{ route('consomations.edit', $item->id) }}" class="btn btn-warning mr-1"><i
+                                class="fa fa-pen"></i></a>
+                        <a href="{{ route('getBons', $item->id) }}" class="btn btn-info mr-1"><i
+                                class="fa fa-eye"></i></a>
+                        <form action="{{ route('consomations.destroy', $item->id) }}" method="post">
+                            @csrf
+                            @method('delete')
+                            <button class="btn btn-danger"><i class="fa fa-trash"></i></button>
+                        </form>
+                    </td>
+                </tr>
             @endforeach
         </tbody>
 
