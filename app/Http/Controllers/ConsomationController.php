ebe3bc508d8b0f942f3b02bae4eec73672ab3f1e
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-08-15 22:36:53 +0100

    Update ConsomationController.php

--- app/Http/Controllers/ConsomationController.php
+++ app/Http/Controllers/ConsomationController.php
@@ -16,7 +16,7 @@ class ConsomationController extends Controller
      */
     public function index()
     {
-        $consomations = Consomation::latest()->paginate(15);
+        $consomations = Consomation::OrderBy('date','desc')->paginate(15);
         return view('gazole.consomation.index', compact('consomations'));
     }
 
