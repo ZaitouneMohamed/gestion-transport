6d9c6e2362f151262be810e423672fbcc4394bd9
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-10-19 14:02:50 +0100

    add images with change of login design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,82 +2,184 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Log in (v2)</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <title>Login Form</title>
+    <style>
+        img {
+            width: 100%;
+        }
 
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
-    <!-- icheck bootstrap -->
-    <link rel="stylesheet" href="{{ asset('adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css') }}">
+        /* .login {
+            height: 1000px;
+            width: 100%;
+            background: radial-gradient(#653d84, #332042);
+            position: relative;
+        } */
+
+        .login_box {
+            width: 1050px;
+            height: 600px;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            background: #fff;
+            border-radius: 10px;
+            box-shadow: 1px 4px 22px -8px #0004;
+            display: flex;
+            overflow: hidden;
+        }
+
+        .login_box .left {
+            width: 41%;
+            height: 100%;
+            padding: 25px 25px;
+        }
+
+        .login_box .right {
+            width: 59%;
+            height: 100%
+        }
+
+        .left .top_link a {
+            color: #452A5A;
+            font-weight: 400;
+        }
+
+        .left .top_link {
+            height: 20px
+        }
+
+        .left .contact {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            align-self: center;
+            height: 100%;
+            width: 73%;
+            margin: auto;
+        }
+
+        .left h3 {
+            text-align: center;
+            margin-bottom: 40px;
+        }
+
+        .left input {
+            border: none;
+            width: 80%;
+            margin: 15px 0px;
+            border-bottom: 1px solid #4f30677d;
+            padding: 7px 9px;
+            width: 100%;
+            overflow: hidden;
+            background: transparent;
+            font-weight: 600;
+            font-size: 14px;
+        }
+
+        .left {
+            background: linear-gradient(-45deg, #dcd7e0, #fff);
+        }
+
+        .submit {
+            border: none;
+            padding: 15px 70px;
+            border-radius: 8px;
+            display: block;
+            margin: auto;
+            margin-top: 120px;
+            background: #2564c2;
+            color: #fff;
+            font-weight: bold;
+            -webkit-box-shadow: 0px 9px 15px -11px rgba(88, 54, 114, 1);
+            -moz-box-shadow: 0px 9px 15px -11px rgba(88, 54, 114, 1);
+            box-shadow: 0px 9px 15px -11px rgba(88, 54, 114, 1);
+        }
+
+
+
+        .right {
+            background: url("http://127.0.0.1:8000/images/global images/Isuzu NPR HD.jpeg");
+            background-size: cover;
+            color: #fff;
+            position: relative;
+        }
+
+        .right .right-text {
+            height: 100%;
+            position: relative;
+            transform: translate(0%, 45%);
+        }
+
+        .right-text h2 {
+            display: block;
+            width: 100%;
+            text-align: center;
+            font-size: 50px;
+            font-weight: 500;
+        }
+
+        .right-text h5 {
+            display: block;
+            width: 100%;
+            text-align: center;
+            font-size: 19px;
+            font-weight: 400;
+        }
+
+        .right .right-inductor {
+            position: absolute;
+            width: 70px;
+            height: 7px;
+            background: #fff0;
+            left: 50%;
+            bottom: 70px;
+            transform: translate(-50%, 0%);
+        }
+
+        .top_link img {
+            width: 28px;
+            padding-right: 7px;
+            margin-top: -3px;
+        }
+    </style>
 </head>
 
-<body class="hold-transition login-page" style="background-image: url('https://akanea.com/wp-content/uploads/2021/10/TMS-2021-article-types-de-transport.jpg'); background-size: cover">
-    <div class="login-box">
-        <!-- /.login-logo -->
-        <div class="card card-outline card-primary">
-            <div class="card-header text-center">
-                <a href="#" style="font-size: 27px ; color: black"><b>aetjtransportetlogistique</b></a>
-            </div>
-            <div class="card-body">
-                <p class="login-box-msg">Sign in to start your session</p>
-
-                <form method="POST" action="{{ route('login_c') }}">
-                    @csrf
-                    @method('POST')
-                    <div class="input-group mb-3">
-                        <input type="email" class="form-control" placeholder="Email" name="email">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-envelope"></span>
-                            </div>
-                        </div>
+<body>
+    <section class="login">
+        <div class="login_box">
+            <div class="left">
+                <div class="contact">
+                    <form method="POST" action="{{ route('login_c') }}">
+                        @csrf
+                        @method('POST')
+                        <img src="{{ asset('images/logo.png') }}" style="width: 60%;margin-left:50px" alt="">
+                        <input type="text" placeholder="EMAIL" name="email">
                         @error('email')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
                             </span>
                         @enderror
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="password" class="form-control" placeholder="Password" name="password">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
-                            </div>
-                        </div>
+                        <input type="password" placeholder="PASSWORD" name="password">
                         @error('password')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
                             </span>
                         @enderror
-                    </div>
-                    <div class="row">
-                        <!-- /.col -->
-                        <div class="col-4">
-                            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-                        </div>
-                        <!-- /.col -->
-                    </div>
-                </form>
+                        <button class="submit">LET'S GO</button>
+                    </form>
+                </div>
             </div>
-            <!-- /.card-body -->
+            <div class="right">
+                <div class="right-inductor"><img src="" alt=""></div>
+            </div>
         </div>
-        <!-- /.card -->
-    </div>
-    <!-- /.login-box -->
-
-    <!-- jQuery -->
-    <script src="{{ asset('adminlte/plugins/jquery/jquery.min.js') }}"></script>
-    <!-- Bootstrap 4 -->
-    <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <!-- AdminLTE App -->
-    <script src="{{ asset('dist/js/adminlte.min.js') }}"></script>
+    </section>
 </body>
 
 </html>
-
