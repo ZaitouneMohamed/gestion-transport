92fdb7f01e807ca7ebe2586b0218232356a37cd1
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-11-14 13:17:36 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::apiResource("chaufeur", ChaufeurController::class);
+Route::apiResource("chaufeur", ChaufeurController::class);
