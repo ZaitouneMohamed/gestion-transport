66cc475e7fa7594fef1b81b08c4ec51205344e54
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-12-08 12:11:59 +0100

    Update index.blade.php

--- resources/views/gazole/index.blade.php
+++ resources/views/gazole/index.blade.php
@@ -89,6 +89,98 @@
                 {{-- <div id="piechart" style="height: 400px;"></div> --}}
                 <canvas id="myPieChart" width="200" height="200"></canvas>
             </div>
+            <div class="col-lg-6 col-6">
+                {{-- <div id="piechart" style="height: 400px;"></div> --}}
+                <table class="table table-striped table-valign-middle">
+                    <thead>
+                        <tr>
+                            <th>Product</th>
+                            <th>Price</th>
+                            <th>Sales</th>
+                            <th>More</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <img src="dist/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
+                                Some Product
+                            </td>
+                            <td>$13 USD</td>
+                            <td>
+                                <small class="text-success mr-1">
+                                    <i class="fas fa-arrow-up"></i>
+                                    12%
+                                </small>
+                                12,000 Sold
+                            </td>
+                            <td>
+                                <a href="#" class="text-muted">
+                                    <i class="fas fa-search"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <img src="dist/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
+                                Another Product
+                            </td>
+                            <td>$29 USD</td>
+                            <td>
+                                <small class="text-warning mr-1">
+                                    <i class="fas fa-arrow-down"></i>
+                                    0.5%
+                                </small>
+                                123,234 Sold
+                            </td>
+                            <td>
+                                <a href="#" class="text-muted">
+                                    <i class="fas fa-search"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <img src="dist/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
+                                Amazing Product
+                            </td>
+                            <td>$1,230 USD</td>
+                            <td>
+                                <small class="text-danger mr-1">
+                                    <i class="fas fa-arrow-down"></i>
+                                    3%
+                                </small>
+                                198 Sold
+                            </td>
+                            <td>
+                                <a href="#" class="text-muted">
+                                    <i class="fas fa-search"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <img src="dist/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
+                                Perfect Item
+                                <span class="badge bg-danger">NEW</span>
+                            </td>
+                            <td>$199 USD</td>
+                            <td>
+                                <small class="text-success mr-1">
+                                    <i class="fas fa-arrow-up"></i>
+                                    63%
+                                </small>
+                                87 Sold
+                            </td>
+                            <td>
+                                <a href="#" class="text-muted">
+                                    <i class="fas fa-search"></i>
+                                </a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 @endsection
@@ -139,7 +231,7 @@
             data: {
                 labels: results.map(function(item) {
                     return monthNames[item.month - 1] + ' ' + item
-                    .year; // Subtract 1 because months are zero-indexed
+                        .year; // Subtract 1 because months are zero-indexed
                 }),
                 datasets: [{
                     label: 'Total Prix',
