64e975b9a52536131ce8cadd01e1f0425d6bf734
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-12-08 11:42:38 +0100

    Update index.blade.php

--- resources/views/gazole/index.blade.php
+++ resources/views/gazole/index.blade.php
@@ -123,9 +123,13 @@
         });
     </script>
     <script>
-        var results = @json($results_2);
+        var results = @json($results);
 
         var ctx = document.getElementById('myColumnChart').getContext('2d');
+        var colors = ['rgba(75, 192, 192, 0.7)', 'rgba(255, 99, 132, 0.7)', 'rgba(255, 205, 86, 0.7)',
+            'rgba(54, 162, 235, 0.7)', 'rgba(153, 102, 255, 0.7)', 'rgba(255, 159, 64, 0.7)'
+        ]; // Add more colors if needed
+
         var myColumnChart = new Chart(ctx, {
             type: 'bar',
             data: {
@@ -135,107 +139,11 @@
                 datasets: [{
                     label: 'Total Prix',
                     data: results.map(item => item.total_prix),
-                    backgroundColor: 'rgba(75, 192, 192, 0.7)', // Adjust the color as needed
+                    backgroundColor: function(context) {
+                        return colors[context.dataIndex % colors.length];
+                    },
                 }],
             },
         });
     </script>
-    {{-- <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script> --}}
-    {{-- <script type="text/javascript">
-        google.charts.load('current', {
-            'packages': ['corechart']
-        });
-        google.charts.setOnLoadCallback(drawChart);
-
-        function drawChart() {
-            var data = google.visualization.arrayToDataTable([
-                ['Task', 'Hours per Day'],
-                ['Work', 11],
-                ['Eat', 2],
-                ['Commute', 2],
-                ['Watch TV', 2],
-                ['Sleep', 7]
-            ]);
-            var options = {
-                title: 'SI Youssef'
-            };
-            var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-            chart.draw(data, options);
-        }
-    </script>
-    <script>
-        google.charts.load('current', {
-            packages: ['corechart', 'bar']
-        });
-        google.charts.setOnLoadCallback(drawStacked);
-
-        function drawStacked() {
-            var data = new google.visualization.DataTable();
-            data.addColumn('timeofday', 'Time of Day');
-            data.addColumn('number', 'Motivation Level');
-            data.addColumn('number', 'Energy Level');
-
-            data.addRows([
-                [{
-                    v: [8, 0, 0],
-                    f: '8 am'
-                }, 1, .25],
-                [{
-                    v: [9, 0, 0],
-                    f: '9 am'
-                }, 2, .5],
-                [{
-                    v: [10, 0, 0],
-                    f: '10 am'
-                }, 3, 1],
-                [{
-                    v: [11, 0, 0],
-                    f: '11 am'
-                }, 4, 2.25],
-                [{
-                    v: [12, 0, 0],
-                    f: '12 pm'
-                }, 5, 2.25],
-                [{
-                    v: [13, 0, 0],
-                    f: '1 pm'
-                }, 6, 3],
-                [{
-                    v: [14, 0, 0],
-                    f: '2 pm'
-                }, 7, 4],
-                [{
-                    v: [15, 0, 0],
-                    f: '3 pm'
-                }, 8, 5.25],
-                [{
-                    v: [16, 0, 0],
-                    f: '4 pm'
-                }, 9, 7.5],
-                [{
-                    v: [17, 0, 0],
-                    f: '5 pm'
-                }, 10, 10],
-            ]);
-
-            var options = {
-                title: 'Motivation and Energy Level Throughout the Day',
-                isStacked: true,
-                hAxis: {
-                    title: 'Time of Day',
-                    format: 'h:mm a',
-                    viewWindow: {
-                        min: [7, 30, 0],
-                        max: [17, 30, 0]
-                    }
-                },
-                vAxis: {
-                    title: 'Rating (scale of 1-10)'
-                }
-            };
-
-            var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
-            chart.draw(data, options);
-        }
-    </script> --}}
 @endsection
