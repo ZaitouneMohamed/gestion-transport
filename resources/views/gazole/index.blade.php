1fd386aa6614730f51b8dcd0ef6e3e2cee438930
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-12-08 11:43:59 +0100

    Update index.blade.php

--- resources/views/gazole/index.blade.php
+++ resources/views/gazole/index.blade.php
@@ -123,7 +123,7 @@
         });
     </script>
     <script>
-        var results = @json($results);
+        var results = @json($results_2);
 
         var ctx = document.getElementById('myColumnChart').getContext('2d');
         var colors = ['rgba(75, 192, 192, 0.7)', 'rgba(255, 99, 132, 0.7)', 'rgba(255, 205, 86, 0.7)',
@@ -146,4 +146,102 @@
             },
         });
     </script>
+    {{-- <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script> --}}
+    {{-- <script type="text/javascript">
+        google.charts.load('current', {
+            'packages': ['corechart']
+        });
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+            var data = google.visualization.arrayToDataTable([
+                ['Task', 'Hours per Day'],
+                ['Work', 11],
+                ['Eat', 2],
+                ['Commute', 2],
+                ['Watch TV', 2],
+                ['Sleep', 7]
+            ]);
+            var options = {
+                title: 'SI Youssef'
+            };
+            var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+            chart.draw(data, options);
+        }
+    </script>
+    <script>
+        google.charts.load('current', {
+            packages: ['corechart', 'bar']
+        });
+        google.charts.setOnLoadCallback(drawStacked);
+
+        function drawStacked() {
+            var data = new google.visualization.DataTable();
+            data.addColumn('timeofday', 'Time of Day');
+            data.addColumn('number', 'Motivation Level');
+            data.addColumn('number', 'Energy Level');
+
+            data.addRows([
+                [{
+                    v: [8, 0, 0],
+                    f: '8 am'
+                }, 1, .25],
+                [{
+                    v: [9, 0, 0],
+                    f: '9 am'
+                }, 2, .5],
+                [{
+                    v: [10, 0, 0],
+                    f: '10 am'
+                }, 3, 1],
+                [{
+                    v: [11, 0, 0],
+                    f: '11 am'
+                }, 4, 2.25],
+                [{
+                    v: [12, 0, 0],
+                    f: '12 pm'
+                }, 5, 2.25],
+                [{
+                    v: [13, 0, 0],
+                    f: '1 pm'
+                }, 6, 3],
+                [{
+                    v: [14, 0, 0],
+                    f: '2 pm'
+                }, 7, 4],
+                [{
+                    v: [15, 0, 0],
+                    f: '3 pm'
+                }, 8, 5.25],
+                [{
+                    v: [16, 0, 0],
+                    f: '4 pm'
+                }, 9, 7.5],
+                [{
+                    v: [17, 0, 0],
+                    f: '5 pm'
+                }, 10, 10],
+            ]);
+
+            var options = {
+                title: 'Motivation and Energy Level Throughout the Day',
+                isStacked: true,
+                hAxis: {
+                    title: 'Time of Day',
+                    format: 'h:mm a',
+                    viewWindow: {
+                        min: [7, 30, 0],
+                        max: [17, 30, 0]
+                    }
+                },
+                vAxis: {
+                    title: 'Rating (scale of 1-10)'
+                }
+            };
+
+            var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+            chart.draw(data, options);
+        }
+    </script> --}}
 @endsection
